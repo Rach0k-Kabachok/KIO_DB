0528c795ee9ffab935628734789e23ee2c845f0a
Ivan Kolotlov <iokolotilov@edu.hse.ru>
2026-05-29 08:12:10 +0300

    update setup.sh. Now it installs dependeces

--- script/setup.sh
+++ script/setup.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
 
+apt-get update
+apt-get install -y cmake build-essential git
+
 mkdir -p "${ROOT_DIR}/build"
 cd "${ROOT_DIR}/build"
 cmake ..
